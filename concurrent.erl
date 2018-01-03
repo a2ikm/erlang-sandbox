a7133139858bbff14a1d3a78a22431af16da387a
Masato Ikeda <masato.ikeda@gmail.com>
2018-01-03 15:24:19 +0900

    concurrent.erl
    http://dukelab.hatenablog.com/entry/2014/04/30/164111

---
+++ concurrent.erl
@@ -0,0 +1,23 @@
+-module(concurrent).
+-export([start/0]).
+start() ->
+  spawn(fun() -> echo(1) end).
+
+echo(NoMsgCnt) ->
+  receive
+    shutdown ->
+      io:format("Shutdown echo~n");
+    {one, Msg} ->
+      io:format("One Msg ~p~n", [Msg]),
+      echo(NoMsgCnt);
+    {two, Msg} ->
+      io:format("Two Msg ~p~n", [Msg]),
+      echo(NoMsgCnt);
+    Unknown ->
+      io:format("Unknown Msg ~p~n", [Unknown]),
+      echo(NoMsgCnt)
+  after
+    7000 ->
+      io:format("No Msg ~p~n", [NoMsgCnt]),
+      echo(NoMsgCnt + 1)
+  end.
